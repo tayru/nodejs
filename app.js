57120c837ff809efa8b5482c6598f62ad58264a4
denis.brudanin@mail.ru <1Fn87jqn>
2022-04-17 21:56:23 +0300

    второй коммит

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const os = require('os')
+const greeting = require('./greeting')
+
+// получим имя текущего пользователя
+let userName = os.userInfo().username
+
+console.log(`Дата запроса: ${greeting.date}`)
+console.log(greeting.getMessage(userName))
